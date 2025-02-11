44f1f88df7131b40741fe127a2ff729f0f3220a5
diimoon1056 <167615525+diimoon1056@users.noreply.github.com>
2025-02-11 14:08:29 +0200

    Update Task2.py

--- Practical3/Task2.py
+++ Practical3/Task2.py
@@ -1,1 +1,12 @@
-
+num1 = int(input("Введіть число для піднесення до степеня: "))
+num2 = int(input("Введіть степінь: "))
+def power_number(num1, num2):
+    result = 1
+    if num2 < 0:
+        num1 =1 / num1
+        num2 = -num2
+    for i in range(int(num2)):
+            result *= num1
+    return result
+        
+print(f"{num1} в степені {num2} дорівнює {power_number(num1, num2)}")
