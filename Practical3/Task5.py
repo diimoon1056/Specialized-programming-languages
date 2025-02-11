a6e3969c3ea0c9221d165d4e508781f3b8f51025
diimoon1056 <167615525+diimoon1056@users.noreply.github.com>
2025-02-11 13:48:58 +0200

    Create Task5.py

---
+++ Practical3/Task5.py
@@ -0,0 +1,5 @@
+upper_word = input("Введіть слова в рядок: ")
+def upper_registr_word(upper_word):
+    upper_word = upper_word.title()
+    return upper_word
+print("Результат:", upper_registr_word(upper_word))
